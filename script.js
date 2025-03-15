24ae9df1e49fb930d57ac83be8d76b4bc75237b2
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-15 11:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function stringChop(str, size) {
   // your code here
+  if(!str)return [];
+  let ans = [];
+ for (let index = 0; index < str.length; index+=size) {
+ 	let s = str.slice(index,index+size);
+ 	ans.push(s);
+ }
+ return ans;
 }
+console.log("Try programiz.pro");
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
